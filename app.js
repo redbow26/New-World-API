a32fc86443a3d00ff471893b10c9ac47b3cbc187
Redbow <tristanleroy26@gmail.com>
2020-08-23 14:14:48 +0200

    Add mongo query for graphql query

--- app.js
+++ app.js
@@ -76,10 +76,40 @@ app.use('/api', graphqlHTTP({
     `),
     rootValue: {
         crafts: args => {
-            return [];
+            // Create the query with the information
+            const query = {}
+            if (args.stationName) {
+                query['station.name'] = args.stationName;
+            } 
+            if (args.tier) {
+                query.tier = args.tier;
+            }
+            // Find the element in the database with the specify query
+            // With return, graphql know this resolver execute async function
+            return Craft.find(query)
+            .then(crafts => {
+                return crafts.map(craft => {
+                    return { ...craft._doc};
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                // Graphql handle the error
+                throw err;
+            });
         },
         craft: args => {
-            return null;
+            // Find the element in the database with the specify query
+            // With return, graphql know this resolver execute async function
+            return Craft.findOne({ name: args.name })
+            .then(craft => {
+                return  { ...craft._doc };
+            })
+            .catch(err => {
+                console.log(err);
+                // Graphql handle the error
+                throw err;
+            });
         },
         createCrafts: args => {
             // Create new data with the craft schema
